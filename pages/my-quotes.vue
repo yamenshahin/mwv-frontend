dec83176f80abeb518da46c38c4e320d84e3dd74
Yamen Shahin <yamenshahin@gmail.com>
2019-09-12 12:30:17 +0200

    Cleanup extra pages

---
+++ pages/my-quotes.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-container class="color-white">
+    <b-row>
+      <b-col md></b-col>
+    </b-row>
+  </b-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      drivers: []
+    }
+  }
+}
+</script>
--- pages/travel-time.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container">
-    <h1>Hello Travel Time</h1>
-  </div>
-</template>
-
-<script>
-export default {}
-</script>
