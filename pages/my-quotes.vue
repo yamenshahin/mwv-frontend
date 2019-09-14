4d96869eb585a984f710f6b4427b778ecbbbd559
Yamen Shahin <yamenshahin@gmail.com>
2019-09-14 05:42:19 +0200

    Add breice breakdown

--- pages/my-quotes.vue
+++ pages/my-quotes.vue
@@ -1,6 +1,7 @@
 <template>
   <b-container>
     <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ searchMetaObject }}</pre>
       <pre class="m-0">{{ searchResultObject }}</pre>
     </b-card>
     <div v-for="(place, index) in places" :key="index">
@@ -39,15 +40,80 @@
                 <strong>£{{ place.price.additionalTimePrice }}</strong>
                 per half hour
               </p>
-              <b-button block variant="info">Book Now</b-button>
-              <a v-b-toggle.1 href="#">a</a>
+              <b-button block variant="info" class="mb-1">Book Now</b-button>
+              <b-button
+                v-b-toggle="'collapse-' + index"
+                variant="danger"
+                size="sm"
+              >
+                Price breakdown
+              </b-button>
+              <b-button variant="danger" size="sm">
+                Email
+              </b-button>
             </b-col>
           </b-row>
         </div>
       </div>
-      <div id="0" class="collapse">
-        <b-card>I should start open!</b-card>
-      </div>
+
+      <b-collapse :id="'collapse-' + index">
+        <b-card>
+          <b-table-simple hover small responsive>
+            <b-thead head-variant="info">
+              <b-tr>
+                <b-th>Item</b-th>
+                <b-th>Price</b-th>
+              </b-tr>
+            </b-thead>
+            <b-tbody>
+              <b-tr>
+                <b-td>Mileage charge</b-td>
+                <b-td>£{{ place.price.estimatedDistancePrice }}</b-td>
+              </b-tr>
+              <b-tr>
+                <b-td>Driver time charge</b-td>
+                <b-td>£{{ place.price.totalTimePrice }}</b-td>
+              </b-tr>
+              <b-tr>
+                <b-td>Total stairs price</b-td>
+                <b-td>£{{ place.price.stairsPrice }}</b-td>
+              </b-tr>
+              <b-tr>
+                <b-td>Subtotal</b-td>
+                <b-td>£{{ place.price.subtotal }}</b-td>
+              </b-tr>
+              <b-tr>
+                <b-td>Fee</b-td>
+                <b-td>£{{ place.price.fee }}</b-td>
+              </b-tr>
+            </b-tbody>
+            <b-tfoot>
+              <b-tr variant="secondary">
+                <b-td>Total</b-td>
+                <b-td>£{{ place.price.total }}</b-td>
+              </b-tr>
+              <b-tr variant="info">
+                <b-td>Estimated distance</b-td>
+                <b-td>{{ searchResultObject.job_meta.milesDriven }} miles</b-td>
+              </b-tr>
+              <b-tr variant="info">
+                <b-td>Estimated driving time</b-td>
+                <b-td>
+                  {{
+                    formateTimeToString(searchResultObject.job_meta.travelTime)
+                  }}
+                </b-td>
+              </b-tr>
+              <b-tr variant="info">
+                <b-td>Any additional time will be charged at</b-td>
+                <b-td>
+                  £{{ place.price.additionalTimePrice }} per half hour
+                </b-td>
+              </b-tr>
+            </b-tfoot>
+          </b-table-simple>
+        </b-card>
+      </b-collapse>
     </div>
   </b-container>
 </template>
@@ -62,6 +128,17 @@ export default {
   mounted() {
     this.jobMeta = this.searchResultObject.job_meta
     this.places = this.searchResultObject.data
+  },
+  methods: {
+    formateTimeToString(timeInSec) {
+      const h = Math.floor(timeInSec / 3600)
+      const m = Math.floor((timeInSec % 3600) / 60)
+
+      const hDisplay = h > 0 ? h + (h === 1 ? ' hour' : ' hours') : ''
+      const mDisplay = m > 0 ? m + (m === 1 ? ' minute, ' : ' minutes ') : ''
+      const coma = hDisplay && mDisplay ? ', ' : ''
+      return hDisplay + coma + mDisplay
+    }
   }
 }
 </script>
